f7e1d782ca8732079afc5bc19827fa0b4306c445
Sultan <sultanZtony@gmail.com>
2023-04-15 14:51:43 -0600

    Enums Added

---
+++ Enums.java
@@ -0,0 +1,11 @@
+// All the enums for the program in one spot
+// I much prefer enums to magic numbers or constants
+
+public class Enums {
+    public enum Health {Injured,Healthy}
+    public enum Equipment {Weapon , Airplane , Tanks}
+    public enum StaffType {Pilots , Soldiers , Tanks driver,Mechanics,Medic, Reserve army}
+    public enum DayOfWeek {Mon,Tue,Wed,Thur,Fri,Sat,Sun}
+    public enum Resources {Gold, Oil , Food, Ammo, Medicine }
+    public enum Countries {Germany , US , UK, France, Italy, Soviet Union}
+}
