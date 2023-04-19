4e853ca152d4faab02f997bd1810330f416f9498
JonathanTaing <Jonathan.Taing@colorado.edu>
2023-04-19 09:00:33 -0600

    Create a basic CLI structure for game

---
+++ Country.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Country implements SysOut {
+    String name;
+
+    Country(String name) {
+        this.name = name;
+    }
+}
--- Simulator.java
+++ Simulator.java
@@ -1,10 +1,14 @@
+// Simulator.java -> Game.java
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-// Simulator to cycle for select number of days
+// Simulator to cycle for select number of years
 public class Simulator implements SysOut {
+    Scanner scanner = new Scanner(System.in);
+    Country playerCountry;
+
     void run() {
         File file = new File("SimResults.txt");
         boolean fileExists = file.exists();
@@ -17,8 +21,55 @@ public class Simulator implements SysOut {
             e.printStackTrace();
         }
 
-        for (int year = 1; year <= 30; ++year) {
+        out("Welcome to the WW2 Strategy Game!");
+        chooseCountry();
+        mainMenu();
+    }
 
+    private void chooseCountry() {
+        out("Please choose a country to play as (enter number): ");
+        out("1. USA");
+        out("2. Germany");
+
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                playerCountry = new Country("USA");
+                break;
+            case 2:
+                playerCountry = new Country("Germany");
+                break;
+            default:
+                out("Invalid selection. Try again.");
+                chooseCountry();
+                return;
+        }
+        out("You chose: " + playerCountry.name);
+    }
+
+    private void mainMenu() {
+        int year = 0;
+        int totalYears = 30;
+
+        while (year < totalYears) {
+            out("\nYear: " + (year+1));
+            out("Please select an option (enter number): ");
+            out("1. Extract Resources");
+            out("2. Quit game");
+
+            int choice = scanner.nextInt();
+            switch(choice) {
+                case 1:
+                    out("Extracting Resources...");
+                    break;
+                case 2:
+                    out("Thanks for playing!");
+                    System.exit(0);
+                default:
+                    out("Invalid selection. Try again.");
+                    continue;
+            }
+            year++;
         }
     }
 }
